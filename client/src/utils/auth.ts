147760d01e84fc6d40924c3ba6c8981ce64da4a0
unknown <ernestoaleman00@gmail.com>
2025-05-03 23:19:33 -0500

    At AuthService class, init loggedIn function

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -8,6 +8,8 @@ class AuthService {
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
